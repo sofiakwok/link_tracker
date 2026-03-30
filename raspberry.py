21390c1170e8683406b68a553f4001f72054954c
sofiakwok <zazakwok@gmail.com>
2026-03-29 20:50:29 -0700

    making a class

--- raspberry.py
+++ raspberry.py
@@ -8,62 +8,63 @@ import time
 from datetime import datetime
 from graphics_display import Graphics
 
-# yes i know this should probably be in a class
+class TransitData():
+    def __init__(self):
+        pass
 
-def get_beacon_hill_stop():
-    # agency ID for one line: 40
-    # 1 line ID: 40_100479
-    # incredibly goofy setup in the API where the north and south trains have diff stop names
-    stop_ids = ["40_99240", '40_C19', '40_99121']
-    stop_data = []
-    for stop_id in stop_ids:
-        # print(stop_id)
-        url = "https://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/" + stop_id + ".json?key=" + secret.api_key
-        # print(url)
-        data = requests.get(f'{url}').json()
-        current_time = int(data["currentTime"])
-        incoming = data["data"]["entry"]["arrivalsAndDepartures"]
-        for services in incoming:
-            if services["routeShortName"] == "1 Line":
-                if services["departureEnabled"]: # services["predicted"] is false right now?
-                    time_to_go = (int(services["predictedArrivalTime"]) - current_time) / 1000 / 60
-                    stops_away = int(services["numberOfStopsAway"])
-                    if stops_away > 0:
-                        print("")
-                        print("predicted: " + str(services["predicted"]))
-                        print("time to go (min): " + str(int(time_to_go)))
-                        closest_stop = str(get_stop_name(services["tripStatus"]["closestStop"]))
-                        print("closest stop: " + closest_stop)
-                        print("stops away: " + str(services["numberOfStopsAway"]))
-                        
-                        # get train direction
-                        dir = get_stop_direction(stop_id=services["tripStatus"]["closestStop"])
-                        if dir:
-                            direction = "North"
-                        else:
-                            direction = "South"
-                        print("direction: " + str(direction))
-                        stop_data.append([0, int(time_to_go), closest_stop, int(services["numberOfStopsAway"]), dir])
-    
-    return stop_data
+    def get_beacon_hill_stop(self):
+        # agency ID for one line: 40
+        # 1 line ID: 40_100479
+        # incredibly goofy setup in the API where the north and south trains have diff stop names
+        stop_ids = ["40_99240", '40_C19', '40_99121']
+        stop_data = []
+        for stop_id in stop_ids:
+            # print(stop_id)
+            url = "https://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/" + stop_id + ".json?key=" + secret.api_key
+            # print(url)
+            data = requests.get(f'{url}').json()
+            current_time = int(data["currentTime"])
+            incoming = data["data"]["entry"]["arrivalsAndDepartures"]
+            for services in incoming:
+                if services["routeShortName"] == "1 Line":
+                    if services["departureEnabled"]: # services["predicted"] is false right now?
+                        time_to_go = (int(services["predictedArrivalTime"]) - current_time) / 1000 / 60
+                        stops_away = int(services["numberOfStopsAway"])
+                        if stops_away > 0:
+                            print("")
+                            print("predicted: " + str(services["predicted"]))
+                            print("time to go (min): " + str(int(time_to_go)))
+                            closest_stop = str(get_stop_name(services["tripStatus"]["closestStop"]))
+                            print("closest stop: " + closest_stop)
+                            print("stops away: " + str(services["numberOfStopsAway"]))
+                            
+                            # get train direction
+                            dir = self.get_stop_direction(stop_id=services["tripStatus"]["closestStop"])
+                            if dir:
+                                direction = "North"
+                            else:
+                                direction = "South"
+                            print("direction: " + str(direction))
+                            stop_data.append([0, int(time_to_go), closest_stop, int(services["numberOfStopsAway"]), dir])
+        return stop_data
 
-def get_stop_direction(stop_id):
-    # get stop value
-    polarizing_stop = 'Beacon Hill'
-    stop_ids_map = np.loadtxt("ordered_stops.txt", delimiter=",", dtype=str)
+    def get_stop_direction(self, stop_id):
+        # get stop value
+        polarizing_stop = 'Beacon Hill'
+        stop_ids_map = np.loadtxt("ordered_stops.txt", delimiter=",", dtype=str)
 
-    # finding stop val range for polarizing stop
-    start_val = 0
-    for stop in stop_ids_map:
-        if stop[1] == polarizing_stop:
-            start_val = int(stop[2])
-            break
+        # finding stop val range for polarizing stop
+        start_val = 0
+        for stop in stop_ids_map:
+            if stop[1] == polarizing_stop:
+                start_val = int(stop[2])
+                break
 
-    # get whether train is coming from north or south
-    for stop in stop_ids_map:
-        if stop_id == stop[0]:
-            # if this is TRUE then the train is going north
-            return int(stop[2]) > start_val
+        # get whether train is coming from north or south
+        for stop in stop_ids_map:
+            if stop_id == stop[0]:
+                # if this is TRUE then the train is going north
+                return int(stop[2]) > start_val
         
 def get_stop_name(stop_id):
     stop_ids_numbered = np.loadtxt("ordered_stops.txt", delimiter=",", dtype=str)
@@ -72,11 +73,12 @@ def get_stop_name(stop_id):
         if stop_id == stop[0]:
             return stop[1]
         
-# getting it to update every 30 seconds. 
+# getting it to update every 30 seconds.
+data = TransitData()
 graphics = Graphics()
 # load_stop_names()
 while True:
-    stop_data = get_beacon_hill_stop()
+    stop_data = data.get_beacon_hill_stop()
     graphics.display_stops(stop_data)
 
     time.sleep(30)
